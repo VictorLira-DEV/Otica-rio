09f5bc60926238df36db73debd5dcb95ff4793db
VictorLira-DEV <victorliradev@gmail.com>
2021-04-05 10:44:14 -0300

    ótica rio

---
+++ map.js
@@ -0,0 +1,13 @@
+var map
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -20.3784033, lng: -40.3686145},
+        zoom: 15,
+    });
+
+    var marker = new google.maps.Marker({
+        position: { lat: -20.3784033, lng: -40.3686145 },
+        map: map
+    });
+   
+}
