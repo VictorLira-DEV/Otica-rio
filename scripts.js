2b38c00d74ebecd840201c60860c47dd2e9d97d5
VictorLira <victorliradev@gmail.com>
2021-04-05 23:10:11 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,33 +12,33 @@ document.querySelector('.nav-list').addEventListener('click', function () {
     navegation.classList.remove('display');
 })
 
-    // Javascript for image slider manual navigation
-    let manualNav = function(manual){
-      slides.forEach((slide) => {
-        slide.classList.remove('active');
+// Javascript for image slider manual navigation
+let manualNav = function(manual){
+    slides.forEach((slide) => {
+    slide.classList.remove('active');
 
-        btns.forEach((btn) => {
-          btn.classList.remove('active');
+     btns.forEach((btn) => {
+        btn.classList.remove('active');
         });
       });
 
-      slides[manual].classList.add('active');
-      btns[manual].classList.add('active');
-    }
+     slides[manual].classList.add('active');
+     btns[manual].classList.add('active');
+}
 
-    btns.forEach((btn, i) => {
-      btn.addEventListener("click", () => {
-        manualNav(i);
-        currentSlide = i;
-      });
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
     });
+});
 
-    // Javascript for image slider autoplay navigation
-    let repeat = function(activeClass){
-      let active = document.getElementsByClassName('active');
-      let i = 1;
+// Javascript for image slider autoplay navigation
+let repeat = function(activeClass){
+    let active = document.getElementsByClassName('active');
+    let i = 1;
 
-      let repeater = () => {
+    let repeater = () => {
         setTimeout(function(){
           [...active].forEach((activeSlide) => {
             activeSlide.classList.remove('active');
@@ -58,5 +58,5 @@ document.querySelector('.nav-list').addEventListener('click', function () {
       }, 5000);
       }
       repeater();
-    }
+}
     repeat();
