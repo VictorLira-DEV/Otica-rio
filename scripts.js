38b2fa34ce0400407664ee1eb36ebb381c139f8c
VictorLira <victorliradev@gmail.com>
2021-05-02 20:24:51 -0300

    formating code

--- scripts.js
+++ scripts.js
@@ -1,62 +1,62 @@
 //start
-let navegation =  document.querySelector('.nav-list');
-let slides = document.querySelectorAll('.slide');
-let btns = document.querySelectorAll('.btn');
+let navegation = document.querySelector(".nav-list");
+let slides = document.querySelectorAll(".slide");
+let btns = document.querySelectorAll(".btn");
 let currentSlide = 1;
 
-document.querySelector('.ion-md-menu').addEventListener('click', function () {
-    navegation.classList.add('display');
-})
+document.querySelector(".ion-md-menu").addEventListener("click", function () {
+  navegation.classList.add("display");
+});
 
-document.querySelector('.nav-list').addEventListener('click', function () {
-    navegation.classList.remove('display');
-})
+document.querySelector(".nav-list").addEventListener("click", function () {
+  navegation.classList.remove("display");
+});
 
 // Javascript for image slider manual navigation
-let manualNav = function(manual){
-    slides.forEach((slide) => {
-    slide.classList.remove('active');
+let manualNav = function (manual) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
 
-     btns.forEach((btn) => {
-        btn.classList.remove('active');
-        });
-      });
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
 
-     slides[manual].classList.add('active');
-     btns[manual].classList.add('active');
-}
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
 
 btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
+  btn.addEventListener("click", () => {
     manualNav(i);
     currentSlide = i;
-    });
+  });
 });
 
 // Javascript for image slider autoplay navigation
-let repeat = function(activeClass){
-    let active = document.getElementsByClassName('active');
-    let i = 1;
+let repeat = function (activeClass) {
+  let active = document.getElementsByClassName("active");
+  let i = 1;
 
-    let repeater = () => {
-        setTimeout(function(){
-          [...active].forEach((activeSlide) => {
-            activeSlide.classList.remove('active');
-          });
+  let repeater = () => {
+    setTimeout(function () {
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove("active");
+      });
 
-        slides[i].classList.add('active');
-        btns[i].classList.add('active');
-        i++;
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
 
-        if(slides.length == i){
-          i = 0;
-        }
-        if(i >= slides.length){
-          return;
-        }
-        repeater();
-      }, 5000);
+      if (slides.length == i) {
+        i = 0;
+      }
+      if (i >= slides.length) {
+        return;
       }
       repeater();
-}
-    repeat();
+    }, 5000);
+  };
+  repeater();
+};
+repeat();
